09db98eea27d5b58485e50792efe6ef6eaf62ee9
Utkarsh Vardhan Singh <vardhanc0007@gmail.com>
2024-06-15 20:47:45 +0530

    Create LLImpQues.java

---
+++ LLImpQues.java
@@ -0,0 +1,31 @@
+// //find nth node from last + delete it
+// import java.util.*;
+
+// public class LLImpQues {
+//     public ListNode removeNthFromEnd(ListNode head, int n) {
+//         if(head.next == null) {
+//             return null;
+//         }
+//         int size = 0;
+//         ListNode curr = head;
+//         while(curr != null) {
+//             curr = curr.next;
+//             size++;
+//         }
+
+//         if(n == size) {
+//             return head.next;
+//         }
+
+//         int indexToSearch = size - n;
+//         ListNode prev = head;
+//         int i = 1;
+//         while(i < indexToSearch) {
+//             prev = prev.next;
+//             i++;
+//         }
+//         prev.next = prev.next.next;
+//         return head;
+//     }
+// }
+
