a06f5943bf0284ff01444915015d93275fe5485a
Utkarsh Vardhan Singh <vardhanc0007@gmail.com>
2024-06-21 19:43:07 +0530

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -431,16 +431,179 @@
 // }
 
 //Undirected graph cycle detection
+// import java.util.*;
+
+// public class Graph {
+//     static class Edge {
+//         int src;
+//         int dest;
+
+//         public Edge(int s, int d) {
+//             this.src = s;
+//             this.dest = d;
+//         }
+//     }
+
+//     static void createGraph(ArrayList<Edge> graph[]) {
+//         for (int i = 0; i < graph.length; i++) {
+//             graph[i] = new ArrayList<>();
+//         }
+//         graph[0].add(new Edge(0, 1));
+//         graph[0].add(new Edge(0, 2));
+//         graph[0].add(new Edge(0, 3));
+
+//         graph[1].add(new Edge(1, 0));
+//         graph[1].add(new Edge(1, 2));
+
+//         graph[2].add(new Edge(2, 0));
+//         graph[2].add(new Edge(2, 1));
+
+//         graph[3].add(new Edge(3, 0));
+//         graph[3].add(new Edge(3, 4));
+
+//         graph[4].add(new Edge(4, 3));
+//     }
+
+//     public static boolean isCyclicUtil(ArrayList<Edge>[] graph, boolean vis[], int curr, int par) {
+//         vis[curr] = true;
+//         for (int i = 0; i < graph[curr].size(); i++) {
+//             Edge e = graph[curr].get(i);
+//             // case1
+//             if (vis[e.dest] && e.dest != par) {
+//                 return true;
+//             } 
+//             else if (!vis[e.dest]) {
+//                 // case 3
+//                 if (isCyclicUtil(graph, vis, e.dest, curr)) {
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+
+//     public static void main(String args[]) {
+//         /*
+//           0 ------- 3
+//         / |         |
+//        /  |         |
+//       1   |         4
+//        \  |
+//         \ |
+//           2
+//          */
+//         int V = 5;
+//         ArrayList<Edge> graph[] = new ArrayList[V];
+//         createGraph(graph);
+//         System.out.println(isCyclicUtil(graph, new boolean[V], 0, -1));
+//     }
+// }
+
+//Dijkstra algorithm
+// import java.security.Permissions;
+// import java.util.*;
+
+// public class Graph {
+//     static class Edge {
+
+//         int src;
+//         int dest;
+//         int wt;
+
+//         public Edge(int s, int d, int w) {
+//             this.src = s;
+//             this.dest = d;
+//             this.wt = w;
+//         }
+//     }
+
+//     static void createGraph(ArrayList<Edge> graph[]) {
+//         for (int i = 0; i < graph.length; i++) {
+//             graph[i] = new ArrayList<>();
+//         }
+//         graph[0].add(new Edge(0, 1, 2));
+//         graph[0].add(new Edge(0, 2, 4));
+
+//         graph[1].add(new Edge(1, 3, 7));
+//         graph[1].add(new Edge(1, 2, 1));
+
+//         graph[2].add(new Edge(2, 4, 3));
+
+//         graph[3].add(new Edge(3, 5, 1));
+
+//         graph[4].add(new Edge(4, 3, 2));
+//         graph[4].add(new Edge(4, 5, 5));
+//     }
+
+//     static class Pair implements Comparable<Pair> {
+//         int n;
+//         int path;
+
+//         public Pair(int n, int path) {
+//             this.n = n;
+//             this.path = path;
+//         }
+
+//         @Override
+//         public int compareTo(Pair p2) {
+//             return this.path - p2.path; //ascending order
+//         }
+//     }
+
+//     public static int[] dijkstra(ArrayList<Edge> graph[], int src) {
+//         PriorityQueue<Pair> pq = new PriorityQueue<>();
+//         int dist[] = new int[graph.length];
+//         boolean vis[] = new boolean[graph.length];
+//         for (int i = 0; i < dist.length; i++) {
+//             if (i != src) {
+//                 dist[i] = Integer.MAX_VALUE;
+//             }
+//         }
+//         pq.add(new Pair(src, 0));
+//         while (!pq.isEmpty()) {
+//             Pair curr = pq.remove();
+//             if (!vis[curr.n]) {
+//                 vis[curr.n] = true;
+//                 for (int i = 0; i < graph[curr.n].size(); i++) {
+//                     Edge e = graph[curr.n].get(i);
+//                     int u = e.src;
+//                     int v = e.dest;
+//                     if (!vis[v] && dist[u] + e.wt < dist[v]) {
+//                         dist[v] = dist[u] + e.wt;
+//                         pq.add(new Pair(v, dist[v]));
+//                     }
+//                 }
+//             }
+//         }
+//         return dist;
+//     }
+
+//     public static void main(String args[]) {
+//         int V = 6;
+//         ArrayList<Edge> graph[] = new ArrayList[V];
+//         createGraph(graph);
+//         int src = 0;
+//         int dist[] = dijkstra(graph, src);
+//         for (int i = 0; i < dist.length; i++) {
+//             System.out.println(dist[i] + " ");
+//         }
+
+//     }
+// }
+
+//Prims Algorithm
 import java.util.*;
 
 public class Graph {
     static class Edge {
         int src;
         int dest;
+        int wt;
 
-        public Edge(int s, int d) {
+        public Edge(int s, int d, int w) {
             this.src = s;
             this.dest = d;
+            this.wt = w;
         }
     }
 
@@ -448,53 +611,63 @@ public class Graph {
         for (int i = 0; i < graph.length; i++) {
             graph[i] = new ArrayList<>();
         }
-        graph[0].add(new Edge(0, 1));
-        graph[0].add(new Edge(0, 2));
-        graph[0].add(new Edge(0, 3));
+        graph[0].add(new Edge(0, 1, 10));
+        graph[0].add(new Edge(0, 2, 15));
+        graph[0].add(new Edge(0, 3, 30));
 
-        graph[1].add(new Edge(1, 0));
-        graph[1].add(new Edge(1, 2));
+        graph[1].add(new Edge(1, 0, 10));
+        graph[1].add(new Edge(1, 3, 40));
 
-        graph[2].add(new Edge(2, 0));
-        graph[2].add(new Edge(2, 1));
+        graph[2].add(new Edge(2, 0, 15));
+        graph[2].add(new Edge(2, 3, 50));
+        
+        graph[3].add(new Edge(3, 1, 40));
+        graph[3].add(new Edge(3, 2, 50));
 
-        graph[3].add(new Edge(3, 0));
-        graph[3].add(new Edge(3, 4));
-
-        graph[4].add(new Edge(4, 3));
     }
 
-    public static boolean isCyclicUtil(ArrayList<Edge>[] graph, boolean vis[], int curr, int par) {
-        vis[curr] = true;
-        for (int i = 0; i < graph[curr].size(); i++) {
-            Edge e = graph[curr].get(i);
-            // case1
-            if (vis[e.dest] && e.dest != par) {
-                return true;
-            } 
-            else if (!vis[e.dest]) {
-                // case 3
-                if (isCyclicUtil(graph, vis, e.dest, curr)) {
-                    return true;
+    static class Pair implements Comparable<Pair> {
+        int v;
+        int wt;
+
+        public Pair(int v, int wt) {
+            this.v = v;
+            this.wt = wt;
+        }
+
+        @Override
+        public int compareTo(Pair p2) {
+            return this.wt - p2.wt;
+        }
+    }
+
+    // O(ElogE)
+    public static void primAlgo(ArrayList<Edge> graph[]) {
+        boolean vis[] = new boolean[graph.length];
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        pq.add(new Pair(0, 0));
+        int cost = 0;
+        while (!pq.isEmpty()) {
+            Pair curr = pq.remove();
+            if (!vis[curr.v]) {
+                vis[curr.v] = true;
+                cost += curr.wt;
+                for (int i = 0; i < graph[curr.v].size(); i++) {
+                    Edge e = graph[curr.v].get(i);
+                    if (!vis[e.dest]) {
+                        pq.add(new Pair(e.dest, e.wt));
+                    }
                 }
             }
         }
-        return false;
+        System.out.println(cost);
     }
 
     public static void main(String args[]) {
-        /*
-         * 0 ------- 3
-         * / | |
-         * / | |
-         * 1 | 4
-         * \ |
-         * \ |
-         * 2
-         */
-        int V = 5;
+
+        int V = 4;
         ArrayList<Edge> graph[] = new ArrayList[V];
         createGraph(graph);
-        System.out.println(isCyclicUtil(graph, new boolean[V], 0, -1));
+        primAlgo(graph);
     }
 }
